7de5e0f9218823e1549140a924717298303fc29d
Andreas Kraus <ak222zt@student.lnu.se>
2014-10-03 13:20:13 +0200

    client update

--- src/se/lnu/course4dv109/client/Client.java
+++ src/se/lnu/course4dv109/client/Client.java
@@ -1,18 +1,12 @@
 package se.lnu.course4dv109.client;
 
-import se.lnu.course4dv109.service.composite.BetCompositeService;
-import service.composite.CompositeService;
+import service.composite.CompositeServiceClient;
 
 public class Client {
 	
 	public static void main(String[] args) {
-		CompositeService client = new CompositeService("se.lnu.course4dv109");
-		
-		int a[][] = {initialize matrix};
-		int b[][] = {initialize matrix};
-		
-		String qosRequirements =“MinCost”;
-		
-		Object result = client.invokeCompositeService(qosRequirement, a, b);
+		CompositeServiceClient client = new CompositeServiceClient("se.lnu.course4dv109");
+				
+		Object result = client.invokeCompositeService("placeBet");
 	}
 }
