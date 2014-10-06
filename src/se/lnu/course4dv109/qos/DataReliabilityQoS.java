ab1a8363e26d1883a8fbeb4a9362936f7d7f829c
Andreas Kraus <ak222zt@student.lnu.se>
2014-10-06 12:58:06 +0200

    QoS

---
+++ src/se/lnu/course4dv109/qos/DataReliabilityQoS.java
@@ -0,0 +1,28 @@
+package se.lnu.course4dv109.qos;
+
+import java.util.HashMap;
+import java.util.List;
+
+import service.auxiliary.ServiceDescription;
+import service.workflow.AbstractQoSRequirement;
+
+
+public class DataReliabilityQoS implements AbstractQoSRequirement {
+
+    @Override
+    public ServiceDescription applyQoSRequirement(List<ServiceDescription> serviceDescriptions) {		
+		for (int i = 0; i < serviceDescriptions.size(); i++) {
+			HashMap<String, Object> properties = serviceDescriptions.get(i).getCustomProperties();
+			
+		    if (properties.containsKey("Transaction")){
+		    	boolean trans = (boolean)properties.get("Transaction");
+		    	
+				if (trans){
+					return serviceDescriptions.get(i);
+				}
+		    }
+		}
+		
+		return null;
+    }
+}
