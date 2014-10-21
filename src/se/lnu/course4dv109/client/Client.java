35dad354b562c91cb6360be4677d9043af1e77d5
Amaury@LOCALHOST <Amaury@LOCALHOST>
2014-10-21 12:41:00 +0200

    Workflow test for loop and client

--- src/se/lnu/course4dv109/client/Client.java
+++ src/se/lnu/course4dv109/client/Client.java
@@ -1,8 +1,10 @@
 package se.lnu.course4dv109.client;
 
+import java.util.Arrays;
 import java.util.List;
 
 import se.lnu.course4dv109.object.Choice;
+import se.lnu.course4dv109.object.Odds;
 import service.composite.CompositeServiceClient;
 
 public class Client {
@@ -13,8 +15,8 @@ public class Client {
 		
 		for(String qosRequirement: qosRequirements){
 		    System.out.println("QoS requirement:" + qosRequirement );
-		    boolean result = (boolean)client.invokeCompositeService(qosRequirement, "3333-3333", 1, Choice.DRAW, 10.0);
-			System.out.print("[4DV109] Result: " + (result ? "true" : "false"));
+		    Odds[] result = (Odds[])client.invokeCompositeService(qosRequirement, "3333-3333", 1, Choice.DRAW, 10.0);
+			System.out.print("[4DV109] Result: " + (Arrays.toString(result)));
 		}
 	}
 }
